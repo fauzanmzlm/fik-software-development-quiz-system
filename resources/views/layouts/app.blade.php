727fd78b488a6fec097842b6cbda9c7357bec920
fauzanmzlm <fauzanmazlam88@gmail.com>
2024-10-15 15:31:47 +0800

    update favicon and system name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,9 +17,9 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <link rel="icon" href=
-"{{ asset('assets/img/favicon.png') }}" type="image/x-icon">
-<!-- Font Awesome for icons -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+"{{ asset('assets/img/quiz-logo-6.png') }}" type="image/x-icon">
+    <!-- Font Awesome for icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <!-- Scripts -->
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <!-- Chart.js -->
