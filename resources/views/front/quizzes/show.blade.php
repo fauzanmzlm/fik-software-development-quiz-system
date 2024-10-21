e264afa627c0769117d7fa3044ef26d7147b3881
fauzanmzlm <fauzanmazlam88@gmail.com>
2024-10-21 10:09:19 +0800

    update

--- resources/views/front/quizzes/show.blade.php
+++ resources/views/front/quizzes/show.blade.php
@@ -22,7 +22,7 @@
                             </span>
                         </div>
                     @else
-                        @if (auth()->user()->role == App\Models\User::ROLE_ADMIN || auth()->user()->role == App\Models\User::ROLE_EDUCATOR)
+                        @if (auth()->user()?->role == App\Models\User::ROLE_ADMIN || auth()->user()?->role == App\Models\User::ROLE_EDUCATOR)
                             <div class="text-white px-6 py-4 border-0 rounded relative mb-4 bg-red-700">
                                 <span class="inline-block align-middle mr-8">
                                     Test is available only for students.
